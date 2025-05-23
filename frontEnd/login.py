650eac4c2049a738d027387e0d6e94d33f3abe1b
teo1021 <134237349+teo1021@users.noreply.github.com>
2025-05-23 16:40:45 -0500

    Update login.py

--- frontEnd/login.py
+++ frontEnd/login.py
@@ -1,20 +1,16 @@
 import streamlit as st
 
-st.title("Bienvenido a la app de login")
-st.write("Este será el frontend de tu proyecto")
+#este es el titulo
 
-import streamlit as st
-from streamlit_login_auth_ui.widgets import __login__
+st.title("Inicio de sesión")
 
-__login__obj = __login__(auth_token = "courier_auth_token", 
-                    company_name = "Shims",
-                    width = 200, height = 250, 
-                    logout_button_name = 'Logout', hide_menu_bool = False, 
-                    hide_footer_bool = False, 
-                    lottie_url = 'https://assets2.lottiefiles.com/packages/lf20_jcikwtux.json')
+# las casillas de datos
+usuario = st.text_input("Nombre de usuario")
+clave = st.text_input("Contraseña", type="password")
 
-LOGGED_IN = __login__obj.build_login_ui()
-
-if LOGGED_IN == True:
-
-    st.markown("Your Streamlit Application Begins here!")
+# el boton con las condiciones de ingreso
+if st.button("Ingresar"):
+    if usuario == "angus" and clave == "1021":
+        st.success("Bienvenido")
+    else:
+        st.error("Usuario o contraseña incorrectos")
